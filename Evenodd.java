5d1fa9842bfeb632c98a8b3f603956db02dff9eb
shanjay07 <54099089+shanjay07@users.noreply.github.com>
2019-08-14 15:15:56 +0530

    Create Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.until.Scanner;
+public class Evenodd
+{
+public static void maun(string args[])
+{
+Scanner reader = new Scanner(System.in);
+System.out.println("Enter a number:");
+int num=reader.nextInt();
+if(num%2==0)
+System.out.println(num + "Is even");
+elseif(num%2!=0)
+System.out.println(num + "Is even");
+else
+System.out.println("INPUT Is INVALID");
+}
+}

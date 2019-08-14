614b6471803c859cbd6fddeecc7e3c13f9893a8f
shanjay07 <54099089+shanjay07@users.noreply.github.com>
2019-08-14 14:46:37 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class pognegzero
+{
+public static void main(string args[])
+{
+int num;
+Scanner sc=new Scanner(System.in);
+System.out.println("ENTER ANY INTEGER NUMBER:");
+num=sc.nextInt();
+if(num>0)
+System.out.println(num + "Is a positive number");
+eles if(num<0)
+System.out.println(num - "Is a negative number");
+else
+System.out.println("Is a ZERO");
+}
+}
